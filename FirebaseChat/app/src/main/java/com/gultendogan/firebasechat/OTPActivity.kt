058c37c054bb366c8de6abfd96c04d765c748fcd
gulten27 <ggdogann00@gmail.com>
2022-08-21 21:34:33 +0300

    commit

---
+++ FirebaseChat/app/src/main/java/com/gultendogan/firebasechat/OTPActivity.kt
@@ -0,0 +1,77 @@
+package com.gultendogan.firebasechat
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.gultendogan.firebasechat.databinding.ActivityOtpactivityBinding
+import android.app.ProgressDialog
+import android.content.Intent
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
+
+class OTPActivity : AppCompatActivity() {
+
+    var binding : ActivityOtpactivityBinding? = null
+    var verificationId:String? = null
+    var auth:FirebaseAuth?=null
+    var dialog : ProgressDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOtpactivityBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        dialog = ProgressDialog(this@OTPActivity)
+        dialog!!.setMessage("Sending OTP...")
+        dialog!!.setCancelable(false)
+        dialog!!.show()
+        auth = FirebaseAuth.getInstance()
+        supportActionBar?.hide()
+        val phoneNumber = intent.getStringExtra("phoneNumber")
+        binding!!.phoneLble.text = "Verify $phoneNumber"
+
+        val options = PhoneAuthOptions.newBuilder(auth!!)
+            .setPhoneNumber(phoneNumber!!)
+            .setTimeout(60L,TimeUnit.SECONDS)
+            .setActivity(this@OTPActivity)
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+                override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onVerificationFailed(p0: FirebaseException) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onCodeSent(
+                    verifyId: String,
+                    forceResendingToken: PhoneAuthProvider.ForceResendingToken) {
+                    super.onCodeSent(verifyId, forceResendingToken)
+                    dialog!!.dismiss()
+                    verificationId = verifyId
+                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0)
+                    binding!!.otpView.requestFocus()
+                }
+            }).build()
+
+        PhoneAuthProvider.verifyPhoneNumber(options)
+        binding!!.otpView.setOtpCompletionListener { otp ->
+            val credential = PhoneAuthProvider.getCredential(verificationId!!, otp)
+            auth!!.signInWithCredential(credential).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@OTPActivity, SetupProfileActivity::class.java)
+                    startActivity(intent)
+                    finishAffinity()
+                } else {
+                    Toast.makeText(this@OTPActivity, "Failed", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+}
---
+++ FirebaseChat/app/src/main/java/com/gultendogan/firebasechat/adapter/MessagesAdapter.kt
@@ -0,0 +1,179 @@
+package com.gultendogan.firebasechat.adapter
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.gultendogan.firebasechat.R
+import com.gultendogan.firebasechat.databinding.DeleteLayoutBinding
+import com.gultendogan.firebasechat.databinding.ReceiveMsgBinding
+import com.gultendogan.firebasechat.databinding.SendMsgBinding
+import com.gultendogan.firebasechat.model.Message
+
+class MessagesAdapter(
+    var context: Context,
+    messages: ArrayList<Message>?,
+    senderRoom:String,
+    receiverRoom:String
+    ):RecyclerView.Adapter<RecyclerView.ViewHolder?>()
+{
+    lateinit var messages:ArrayList<Message>
+    val ITEM_SENT = 1
+    val ITEM_RECEIVE = 2
+    val senderRoom : String
+    val receiverRoom : String
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return if(viewType == ITEM_SENT){
+            val view = LayoutInflater.from(context).inflate(R.layout.send_msg,parent,false)
+            SentMsgHolder(view)
+        }
+        else{
+            val view = LayoutInflater.from(context).inflate(R.layout.receive_msg,parent,false)
+            ReceiveMsgHolder(view)
+
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val messages = messages[position]
+        return if(FirebaseAuth.getInstance().uid == messages.senderId){
+            ITEM_SENT
+        }else{
+            ITEM_RECEIVE
+        }
+    }
+
+    override fun getItemCount(): Int = messages.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val message = messages[position]
+
+        if (holder.javaClass == SentMsgHolder::class.java){
+
+            val viewHolder = holder as SentMsgHolder
+            if (message.message.equals("photo")){
+                viewHolder.binding.image.visibility = View.VISIBLE
+                viewHolder.binding.message.visibility= View.GONE
+                viewHolder.binding.mLiner.visibility= View.GONE
+                Glide.with(context)
+                    .load(message.imageUrl)
+                    .placeholder(R.drawable.placeholder)
+                    .into(viewHolder.binding.image)
+            }
+            viewHolder.binding.message.text = message.message
+            viewHolder.itemView.setOnLongClickListener {
+                val view = LayoutInflater.from(context).inflate(R.layout.delete_layout,null)
+                val binding:DeleteLayoutBinding = DeleteLayoutBinding.bind(view)
+                val dialog = AlertDialog.Builder(context)
+                    .setTitle("Delete Message")
+                    .setView(binding.root)
+                    .create()
+                binding.everyone.setOnClickListener {
+                    message.message = "This message is removed"
+                    message.messageId?.let { it1->
+                        FirebaseDatabase.getInstance().reference.child("chats")
+                            .child(receiverRoom).child("message").child(it1).setValue(message)
+                    }
+                    message.messageId.let { it1->
+                        if (it1 != null) {
+                            FirebaseDatabase.getInstance().reference.child("chats")
+                                .child(senderRoom).child("message").child(it1).setValue(message)
+                        }
+                    }
+
+                    dialog.dismiss()
+                }
+
+                binding.delete.setOnClickListener {
+                    message.messageId.let { it1->
+                        FirebaseDatabase.getInstance().reference.child("chats")
+                            .child(senderRoom).child("message").child(it1!!).setValue(null)
+                    }
+                    dialog.dismiss()
+                }
+
+                binding.cancel.setOnClickListener { dialog.dismiss() }
+                dialog.show()
+                false
+            }
+
+        }
+
+        else{
+            val viewHolder = holder as ReceiveMsgHolder
+            if (message.message.equals("photo")){
+                viewHolder.binding.image.visibility = View.VISIBLE
+                viewHolder.binding.message.visibility= View.GONE
+                viewHolder.binding.mLiner.visibility= View.GONE
+                Glide.with(context)
+                    .load(message.imageUrl)
+                    .placeholder(R.drawable.placeholder)
+                    .into(viewHolder.binding.image)
+            }
+
+            viewHolder.binding.message.text = message.message
+            viewHolder.itemView.setOnLongClickListener {
+                val view = LayoutInflater.from(context).inflate(R.layout.delete_layout,null)
+                val binding:DeleteLayoutBinding = DeleteLayoutBinding.bind(view)
+                val dialog = AlertDialog.Builder(context)
+                    .setTitle("Delete Message")
+                    .setView(binding.root)
+                    .create()
+                binding.everyone.setOnClickListener {
+                    message.message = "This message is removed"
+                    message.messageId?.let { it1->
+                        FirebaseDatabase.getInstance().reference.child("chats")
+                            .child(senderRoom).child("message").child(it1).setValue(message)
+                    }
+                    message.messageId.let { it1->
+                        FirebaseDatabase.getInstance().reference.child("chats")
+                            .child(receiverRoom).child("message").child(it1!!).setValue(message)
+                    }
+                    dialog.dismiss()
+                }
+
+                binding.delete.setOnClickListener {
+                    message.messageId.let { it1->
+                        FirebaseDatabase.getInstance().reference.child("chats")
+                            .child(senderRoom).child("message").child(it1!!).setValue(null)
+                    }
+                    dialog.dismiss()
+                }
+
+                binding.cancel.setOnClickListener { dialog.dismiss() }
+                dialog.show()
+                false
+            }
+
+        }
+
+
+    }
+
+
+
+    inner class SentMsgHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+        var binding : SendMsgBinding = SendMsgBinding.bind(itemView)
+    }
+
+    inner class ReceiveMsgHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+        var binding : ReceiveMsgBinding = ReceiveMsgBinding.bind(itemView)
+    }
+
+    init {
+        if (messages != null){
+            this.messages = messages
+        }
+        this.senderRoom = senderRoom
+        this.receiverRoom = receiverRoom
+    }
+
+
+}
